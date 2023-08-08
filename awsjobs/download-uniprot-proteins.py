06f92c9bbf1f55f4edb3722b1b3e00e0ccbefac0
Shuvrajyoti Debroy <shuv.sdr@gmail.com>
2023-08-08 12:14:07 -0400

    Synchronizing job download-uniprot-proteins from AWS Glue at 2023/08/08 16:14:07

--- awsjobs/download-uniprot-proteins.py
+++ awsjobs/download-uniprot-proteins.py
@@ -1,6 +1,4 @@
 # Import libraries
-import os
-import csv
 import requests
 import boto3
 from botocore.exceptions import ClientError
@@ -26,56 +24,68 @@ def get_protein_ids(bucket_name, file_name):
         return None
 
 # Download a single file and save it to S3 bucket
-def download_and_upload_file(protein_files_bucket, id):
-    protein_file_url = f'https://www.uniprot.org/uniprotkb/{id}.json'
-    uniprot_file_name = f'uniprot_source_files/{id}.json'
+def download_uniprot_files(bucket_name, folder_name, protein_id):
+    uniprot_file_name = f'{protein_id}.json'
+    uniprot_file_url = f'https://www.uniprot.org/uniprotkb/{uniprot_file_name}'
+    uniprot_file_path = f'{folder_name}/{uniprot_file_name}'
     
     try:
-        response = requests.get(protein_file_url)
+        response = requests.get(uniprot_file_url)
         if response.status_code == 200:
+            print(f'Downloading Uniprot protein file {uniprot_file_url} ...\n')
             json_content = response.content
             response = s3_client.put_object(
-                Bucket=protein_files_bucket,
-                Key=uniprot_file_name,
+                Bucket=bucket_name,
+                Key=uniprot_file_path,
                 Body=json_content
             )
             if response:
-                print(f"Uniprot file {id}.json downloaded successfully to location {protein_files_bucket}/uniprot_source_files/")
-            return id
+                print(f'{uniprot_file_name} downloaded successfully and saved to {bucket_name}/{uniprot_file_path}')
+            return protein_id
         else:
-            logger.error(f"Failed to download {protein_file_url}: HTTP status code {response.status_code}")
+            logger.error(f"Failed to download {uniprot_file_url}: HTTP status code {response.status_code}")
             return None
     except Exception as e:
-        logger.exception(f"An error occurred while processing protein ID {id}: {str(e)}")
+        logger.exception(f"An error occurred while processing Protein ID {protein_id}: {str(e)}")
         return None
 
 def main():
-    # S3 buckets
-    protein_files_bucket = 'shuv-protein-reference-files'
+    # S3 info
+    proteins_reference_bucket = 'proteins-reference'
+    uniprot_proteins_bucket = 'uniprot-proteins'
+    uniprot_proteins_folder = 'uniprot_protein_files'
     
     # Get all protein ids
-    protein_ids = get_protein_ids(protein_files_bucket, 'protein_ids.csv')
-    total_files = len(protein_ids)
-    if protein_ids is None:
+    protein_ids = get_protein_ids(proteins_reference_bucket, 'protein_ids.csv')
+    if protein_ids:
+        total_files = len(protein_ids)
+    else:
         print("No protein ids found!")
         return
     
-    downloaded_files = 0
+    processed_files = 0
+    downloaded = 0
+    failed = 0
     # Download and upload files using multi-threading
     with ThreadPoolExecutor() as executor:
-        future_to_id = {executor.submit(download_and_upload_file, protein_files_bucket, id): id for id in protein_ids}
+        future_to_id = {executor.submit(download_uniprot_files, uniprot_proteins_bucket, uniprot_proteins_folder, protein_id): protein_id for protein_id in protein_ids}
         for future in as_completed(future_to_id):
-            id = future_to_id[future]
+            protein_id = future_to_id[future]
             try:
                 data = future.result()
                 if data:
-                    downloaded_files += 1
-                    print(f"Processing of protein ID {id} completed successfully.")
-                    print(f"Total files downloaded: {downloaded_files}/{total_files}\n")
+                    downloaded += 1
+                    print(f"Processing of Protein ID {protein_id} completed successfully.")
                 else:
-                    logger.error(f"Processing of protein ID {id} failed.")
+                    failed +=1
+                    logger.error(f"Processing of Protein ID {protein_id} failed.")
             except Exception as e:
-                logger.exception(f"An error occurred while processing protein ID {id}: {str(e)}")
+                failed +=1
+                logger.exception(f"An error occurred while processing Protein ID {protein_id}: {str(e)}")
+                
+            processed_files += 1
+            print(f"\nTotal files processed: {processed_files}/{total_files}")
+            print(f"Downloaded: {downloaded}\tFailed: {failed}\n")
 
 if __name__ == "__main__":
     main()
