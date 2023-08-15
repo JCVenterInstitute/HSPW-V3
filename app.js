ea628f1530f49ebee559d7b925516ac4aeaf1d1f
choiwhmarco <whchoi@ucsd.edu>
2023-08-15 10:22:56 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -147,10 +147,39 @@ async function bulk1(file){
   let temp1 = [];
   let temp2 = "";
   let temp3 = "";
-  let json = require('/Users/wchoi/backend/json/study/'+'study4137.json');
-
+  let json = require('/Users/wchoi/backend/json/study2/'+file);
+  
   temp.push({"index":{"_index":"study","_id":json["Samples"][0]["experiment_id_key"]}});
   temp.push(json);
+
+  let j = 0;
+  var client = await getClient();
+
+  const addJson = temp.map(JSON.stringify).join('\n') + '\n';
+  client.bulk({ body: addJson }, (err, data) => {
+  if (err) {
+    console.log(temp);
+    console.error(err);
+  } else {
+    j++;
+    console.log(j);
+    console.log(addJson);
+  }
+});
+}
+
+async function bulk2(file){
+  let temp = [];
+  let temp1 = [];
+  let temp2 = "";
+  let temp3 = "";
+  let json = require('/Users/wchoi/backend/json/peptide2/'+file);
+
+
+  for (let i = 0; i <json["Study Peptide"].length;i++){
+    temp.push({"index":{"_index":"peptide","_id":json["Study Peptide"][i]["experiment_id_key"]+'_'+json["Study Peptide"][i]["Uniprot_accession"]+'_'+json["Study Peptide"][i]["charge"]+'_'+json["Study Peptide"][i]["mz_ratio"]+'_'+json["Study Peptide"][i]["peptide_sequence"]+'_'+json["Study Peptide"][i]["peptide_score"]}});
+    temp.push(json["Study Peptide"][i]);
+  }
 
   let j = 0;
   var client = await getClient();
@@ -162,34 +191,39 @@ async function bulk1(file){
   } else {
     j++;
     console.log(j);
-    console.log(data);
   }
 });
 }
 
 
-/*
-const directoryPath = path.join(__dirname,'/json/study');
+async function count(file){
+  let json = require('/Users/wchoi/backend/json/peptide/'+file);
+  console.log(typeof(json["Study Peptide"].length));
+}
+
+let m = 0;
+const directoryPath = path.join(__dirname,'/json/peptide2');
 //passsing directoryPath and callback function
 let i = [];
 
 fs.readdir(directoryPath, function (err, files) {
     //handling error
     if (err) {
+        console.log(file);
         return console.log('Unable to scan directory: ' + err);
     } 
     //listing all files using forEach
       files.forEach(function (file) {
         // Do whatever you want to do with the file
-      bulk1(file).catch(console.log);
-
+        bulk2(file).catch(console.log);
     });
 
-  
 });
-*/ 
 
-bulk1().catch(console.log);
+
+
+//bulk2().catch(console.log);
+//bulk1().catch(console.log);
 //index().catch(console.log);
 
 //bulk().catch(console.log);
