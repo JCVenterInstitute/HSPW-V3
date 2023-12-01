0f222fe3130ff796fcd785b29e76cf18edbcaabc
Link Wu <iwu@jcvi.org>
2023-12-01 16:19:26 -0500

    Added conversion from csv to tsv in text format

--- src/server/utils/processFile.js
+++ src/server/utils/processFile.js
@@ -4,6 +4,8 @@ const path = require("path");
 exports.processFile = async (inputData, workingDirectory) => {
   console.log("> Processing File Data");
 
+  // Convert csv to tsv
+  const tsv = input.replace(",", "\t");
   // Save to file
   const filePath = path.join(workingDirectory, "inputdata.txt");
   fs.writeFileSync(filePath, inputData);
