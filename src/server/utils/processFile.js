eff5db7d2f5b8e84d772b4c0d951bf648ef2885b
Link Wu <iwu@jcvi.org>
2023-12-01 16:21:21 -0500

    Fixed inputData typo

--- src/server/utils/processFile.js
+++ src/server/utils/processFile.js
@@ -5,7 +5,7 @@ exports.processFile = async (inputData, workingDirectory) => {
   console.log("> Processing File Data");
 
   // Convert csv to tsv
-  const tsv = input.replace(",", "\t");
+  const tsv = inputData.replace(",", "\t");
   // Save to file
   const filePath = path.join(workingDirectory, "inputdata.txt");
   fs.writeFileSync(filePath, tsv);
