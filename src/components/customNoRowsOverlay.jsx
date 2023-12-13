799381c8f4506effc5fa90ed25f5a2f033193f3d
Mickey Zheng <mzheng@jcvi.org>
2023-12-13 15:40:17 -0500

    Removed div around custom overlays

--- src/components/customNoRowsOverlay.jsx
+++ src/components/customNoRowsOverlay.jsx
@@ -1,9 +1,5 @@
-import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+
 export default () => {
-  return (
-    <div className="ag-overlay-loading-center">
-      <CircularProgress />
-    </div>
-  );
+  return <CircularProgress />;
 };
