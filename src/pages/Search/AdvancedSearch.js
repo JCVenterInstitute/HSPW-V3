7fc21306af36c21680edb1da7f56814e7b85400d
Link Wu <iwu@jcvi.org>
2023-12-15 12:05:30 -0500

    Fetching all data for Annotations

--- src/pages/Search/AdvancedSearch.js
+++ src/pages/Search/AdvancedSearch.js
@@ -19,6 +19,7 @@ import SelectAllTransferList from "../../components/Search/SelectAllTransferList
 import CircleCheckedFilled from "@mui/icons-material/CheckCircle";
 import CircleUnchecked from "@mui/icons-material/RadioButtonUnchecked";
 import SearchResultsTable from "../../components/Search/SearchResultsTable";
+import AnnotationsSearchResultsTable from "../../components/Search/AnnotationsSearchResultsTable";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
@@ -604,78 +605,41 @@ const AdvancedSearch = () => {
             Reset
           </Button>
         </Box>
-        {searchStarted && (
-          <Box
-            component="fieldset"
-            sx={{ p: 2, mb: 2, mt: 3 }}
-          >
-            <legend
-              style={{
-                fontSize: "100%",
-                backgroundColor: "#e5e5e5",
-                color: "#222",
-                padding: "0.1em 0.5em",
-                border: "2px solid #d8d8d8",
-              }}
+        {searchStarted &&
+          (entity !== "Annotations" ? (
+            <Box
+              component="fieldset"
+              sx={{ p: 2, mb: 2, mt: 3 }}
             >
-              Search Results
-            </legend>
-            <Box sx={{ display: "flex" }}>
-              <Box
-                sx={{
-                  textAlign: "right",
-                  justifyContent: "flex-end", // To push content to the right
-                  flexGrow: 1, // To make the right Box occupy remaining space
+              <legend
+                style={{
+                  fontSize: "100%",
+                  backgroundColor: "#e5e5e5",
+                  color: "#222",
+                  padding: "0.1em 0.5em",
+                  border: "2px solid #d8d8d8",
                 }}
               >
-                <Typography
-                  display="inline"
+                Search Results
+              </legend>
+              <Box sx={{ display: "flex" }}>
+                <Box
                   sx={{
-                    fontFamily: "Lato",
-                    fontSize: "18px",
-                    color: "#464646",
+                    textAlign: "right",
+                    justifyContent: "flex-end", // To push content to the right
+                    flexGrow: 1, // To make the right Box occupy remaining space
                   }}
                 >
-                  Records Per Page
-                </Typography>
-                <TextField
-                  select
-                  size="small"
-                  InputProps={{
-                    style: {
-                      borderRadius: "10px",
-                    },
-                  }}
-                  value={recordsPerPage}
-                  onChange={(event) => {
-                    const newRecordsPerPage = event.target.value;
-                    setRecordsPerPage(newRecordsPerPage);
-                    gridApi.showLoadingOverlay();
-                    setCurrentPage(1);
-                    handleSearch(1, newRecordsPerPage);
-                  }}
-                  sx={{ marginLeft: "10px", marginRight: "30px" }}
-                >
-                  {[50, 100, 500, 1000].map((option) => (
-                    <MenuItem
-                      key={option}
-                      value={option}
-                    >
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-                <Typography
-                  display="inline"
-                  sx={{
-                    fontFamily: "Lato",
-                    fontSize: "18px",
-                    color: "#464646",
-                  }}
-                >
-                  Page
-                </Typography>
-                {searchResults && (
+                  <Typography
+                    display="inline"
+                    sx={{
+                      fontFamily: "Lato",
+                      fontSize: "18px",
+                      color: "#464646",
+                    }}
+                  >
+                    Records Per Page
+                  </Typography>
                   <TextField
                     select
                     size="small"
@@ -684,108 +648,173 @@ const AdvancedSearch = () => {
                         borderRadius: "10px",
                       },
                     }}
-                    value={currentPage}
-                    sx={{ marginLeft: "10px", marginRight: "10px" }}
-                    onChange={(e) => handlePageChange(e.target.value)}
+                    value={recordsPerPage}
+                    onChange={(event) => {
+                      const newRecordsPerPage = event.target.value;
+                      setRecordsPerPage(newRecordsPerPage);
+                      gridApi.showLoadingOverlay();
+                      setCurrentPage(1);
+                      handleSearch(1, newRecordsPerPage);
+                    }}
+                    sx={{ marginLeft: "10px", marginRight: "30px" }}
                   >
-                    {Array.from({ length: totalPages }, (_, index) => (
+                    {[50, 100, 500, 1000].map((option) => (
                       <MenuItem
-                        key={index + 1}
-                        value={index + 1}
+                        key={option}
+                        value={option}
                       >
-                        {index + 1}
+                        {option}
                       </MenuItem>
                     ))}
                   </TextField>
-                )}
-                <Typography
-                  display="inline"
-                  sx={{
-                    fontFamily: "Lato",
-                    fontSize: "18px",
-                    color: "#464646",
-                    marginRight: "30px",
-                  }}
-                >
-                  out of {totalPages}
-                </Typography>
-                <button
-                  onClick={() => handlePageChange(currentPage - 1)}
-                  disabled={currentPage === 1}
-                  style={{
-                    color: currentPage === 1 ? "#D3D3D3" : "#F6921E",
-                    background: "white",
-                    fontSize: "20px",
-                    border: "none",
-                    cursor: currentPage === 1 ? "default" : "pointer",
-                    transition: currentPage === 1 ? "none" : "background 0.3s",
-                    borderRadius: "5px",
-                    marginRight: "15px",
-                    pointerEvents: currentPage === 1 ? "none" : "auto",
-                    paddingBottom: "5px",
-                  }}
-                  onMouseEnter={(e) =>
-                    (e.currentTarget.style.background =
-                      "rgba(246, 146, 30, 0.2)")
-                  }
-                  onMouseLeave={(e) =>
-                    (e.currentTarget.style.background = "white")
-                  }
-                >
-                  <ArrowBackIosIcon
-                    style={{
-                      display: "inline",
-                      position: "relative",
-                      top: "0.2em",
-                      fontWeight: "bold",
+                  <Typography
+                    display="inline"
+                    sx={{
+                      fontFamily: "Lato",
+                      fontSize: "18px",
+                      color: "#464646",
                     }}
-                  />
-                  prev
-                </button>
-                <button
-                  onClick={() => handlePageChange(currentPage + 1)}
-                  disabled={currentPage === totalPages}
-                  style={{
-                    color: currentPage === totalPages ? "#D3D3D3" : "#F6921E",
-                    background: "white",
-                    fontSize: "20px",
-                    border: "none",
-                    cursor: currentPage === totalPages ? "default" : "pointer",
-                    transition:
-                      currentPage === totalPages ? "none" : "background 0.3s",
-                    borderRadius: "5px",
-                    pointerEvents: currentPage === totalPages ? "none" : "auto",
-                    paddingBottom: "5px",
-                  }}
-                  onMouseEnter={(e) => {
-                    e.currentTarget.style.background =
-                      "rgba(246, 146, 30, 0.2)";
-                  }}
-                  onMouseLeave={(e) =>
-                    (e.currentTarget.style.background = "white")
-                  }
-                >
-                  next
-                  <ArrowForwardIosIcon
-                    style={{
-                      display: "inline",
-                      position: "relative",
-                      top: "0.2em",
-                      fontWeight: "bold",
+                  >
+                    Page
+                  </Typography>
+                  {searchResults && (
+                    <TextField
+                      select
+                      size="small"
+                      InputProps={{
+                        style: {
+                          borderRadius: "10px",
+                        },
+                      }}
+                      value={currentPage}
+                      sx={{ marginLeft: "10px", marginRight: "10px" }}
+                      onChange={(e) => handlePageChange(e.target.value)}
+                    >
+                      {Array.from({ length: totalPages }, (_, index) => (
+                        <MenuItem
+                          key={index + 1}
+                          value={index + 1}
+                        >
+                          {index + 1}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  )}
+                  <Typography
+                    display="inline"
+                    sx={{
+                      fontFamily: "Lato",
+                      fontSize: "18px",
+                      color: "#464646",
+                      marginRight: "30px",
                     }}
-                  />
-                </button>
+                  >
+                    out of {totalPages}
+                  </Typography>
+                  <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    style={{
+                      color: currentPage === 1 ? "#D3D3D3" : "#F6921E",
+                      background: "white",
+                      fontSize: "20px",
+                      border: "none",
+                      cursor: currentPage === 1 ? "default" : "pointer",
+                      transition:
+                        currentPage === 1 ? "none" : "background 0.3s",
+                      borderRadius: "5px",
+                      marginRight: "15px",
+                      pointerEvents: currentPage === 1 ? "none" : "auto",
+                      paddingBottom: "5px",
+                    }}
+                    onMouseEnter={(e) =>
+                      (e.currentTarget.style.background =
+                        "rgba(246, 146, 30, 0.2)")
+                    }
+                    onMouseLeave={(e) =>
+                      (e.currentTarget.style.background = "white")
+                    }
+                  >
+                    <ArrowBackIosIcon
+                      style={{
+                        display: "inline",
+                        position: "relative",
+                        top: "0.2em",
+                        fontWeight: "bold",
+                      }}
+                    />
+                    prev
+                  </button>
+                  <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                    style={{
+                      color: currentPage === totalPages ? "#D3D3D3" : "#F6921E",
+                      background: "white",
+                      fontSize: "20px",
+                      border: "none",
+                      cursor:
+                        currentPage === totalPages ? "default" : "pointer",
+                      transition:
+                        currentPage === totalPages ? "none" : "background 0.3s",
+                      borderRadius: "5px",
+                      pointerEvents:
+                        currentPage === totalPages ? "none" : "auto",
+                      paddingBottom: "5px",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.currentTarget.style.background =
+                        "rgba(246, 146, 30, 0.2)";
+                    }}
+                    onMouseLeave={(e) =>
+                      (e.currentTarget.style.background = "white")
+                    }
+                  >
+                    next
+                    <ArrowForwardIosIcon
+                      style={{
+                        display: "inline",
+                        position: "relative",
+                        top: "0.2em",
+                        fontWeight: "bold",
+                      }}
+                    />
+                  </button>
+                </Box>
               </Box>
+              <SearchResultsTable
+                entity={entity}
+                searchResults={searchResults}
+                columnDefs={columnDefs}
+                recordsPerPage={recordsPerPage}
+                handleGridApiChange={handleGridApiChange}
+              />
             </Box>
-            <SearchResultsTable
-              entity={entity}
-              searchResults={searchResults}
-              columnDefs={columnDefs}
-              recordsPerPage={recordsPerPage}
-              handleGridApiChange={handleGridApiChange}
-            />
-          </Box>
-        )}
+          ) : (
+            <Box
+              component="fieldset"
+              sx={{ p: 2, mb: 2, mt: 3 }}
+            >
+              <legend
+                style={{
+                  fontSize: "100%",
+                  backgroundColor: "#e5e5e5",
+                  color: "#222",
+                  padding: "0.1em 0.5em",
+                  border: "2px solid #d8d8d8",
+                }}
+              >
+                Search Results
+              </legend>
+              <AnnotationsSearchResultsTable
+                entity={entity}
+                searchResults={searchResults}
+                columnDefs={columnDefs}
+                recordsPerPage={recordsPerPage}
+                handleGridApiChange={handleGridApiChange}
+              />
+            </Box>
+          ))}
       </Container>
     </>
   );
