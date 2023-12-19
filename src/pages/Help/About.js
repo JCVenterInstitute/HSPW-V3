3c9004c3ee07f2f32862c4270848be725924005e
Link Wu <iwu@jcvi.org>
2023-12-19 13:45:29 -0500

    Finished about page

--- src/pages/Help/About.js
+++ src/pages/Help/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import main_feature from "../../assets/hero.jpeg";
 import { Typography, Container } from "@mui/material";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -30,29 +32,27 @@ const About = () => {
             paddingRight: "40px",
           }}
         >
-          Human Salivary Proteome Project
-        </p>
-      </div>
-      <Container style={{ marginTop: "10px" }}>
-        <Typography>
           For many decades, researchers have known that saliva is important for
           chewing, tasting, swallowing, and as the first step in digestion. A
           multitude of proteins and other molecules present in saliva also play
           vital roles in control of viral and bacterial functions in the context
           of health and disease.
-        </Typography>
+        </p>
+      </div>
+      <Container sx={{ mt: 4 }}>
         <Typography
           variant="h5"
           sx={{
+            fontFamily: "Montserrat",
             fontWeight: "bold",
-            mt: 3,
+            mt: 4,
+            mb: 3,
             color: "black",
-            marginBottom: "10px",
           }}
         >
           Human Salivary Proteome Project
         </Typography>
-        <Typography>
+        <Typography sx={{ fontFamily: "Lato", fontSize: "18px" }}>
           New evidence improved our understanding of salivary contributions to
           human health. In March 2008, an NIDCR-supported team of biologists,
           chemists, engineers and computer scientists at five research
@@ -66,10 +66,17 @@ const About = () => {
           <a
             href="https://pubmed.ncbi.nlm.nih.gov/18361515/"
             title="PubMed:18361515"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
           >
             this seminal paper
+            <OpenInNewIcon sx={{ fontSize: "medium" }} />
           </a>
-          . This work was supported mainly by U.S. Public Health Service Grants
+          .
+        </Typography>
+        <Typography sx={{ mt: 2, fontFamily: "Lato", fontSize: "18px" }}>
+          This work was supported mainly by U.S. Public Health Service Grants
           R01 DE016937-16 (Parent Grant PI: Floyd Dewhirst, Forsyth; Supplement
           Consortium PI: Marcelo Freire, JCVI) from the National Institutes of
           Health/National Institute of Dental and Craniofacial Research.
@@ -77,16 +84,16 @@ const About = () => {
         <Typography
           variant="h5"
           sx={{
+            fontFamily: "Montserrat",
             fontWeight: "bold",
-            mt: 3,
+            mt: 4,
+            mb: 3,
             color: "black",
-            marginBottom: "10px",
-            marginTop: "10px",
           }}
         >
           A Community-based Web Portal
         </Typography>
-        <Typography>
+        <Typography sx={{ fontFamily: "Lato", fontSize: "18px" }}>
           The experimental data from the project as well as information from
           popular knowledge bases such as UniProt and PubMed are now available
           online in the Human Salivary Proteome Wiki (HSPW). A group of
@@ -94,33 +101,53 @@ const About = () => {
           various datasets to develop the{" "}
           <a
             href="https://journals.sagepub.com/doi/abs/10.1177/00220345211014432?journalCode=jdrb"
-            class="external text"
-            rel="nofollow"
             target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
           >
             HSPW
+            <OpenInNewIcon sx={{ fontSize: "medium" }} />
           </a>
           . Saliva tests based on these biomarkers offer many advantages over
           blood tests that require a needle stick and can pose contamination
           risks from blood-borne diseases. However, much effort is still
-          required to enrich and refine the salivary ontology and functions. The
-          HSPW committee members are recognized researchers in the oral biology
-          community. They are tasked to develop curation guidelines, review
-          annotations submitted by the community, and promote the wiki. Click
-          here to see who they are. To improve our efforts to a functional era
-          of big data, our team has joined the team from{" "}
+          required to enrich and refine the salivary ontology and functions.
+        </Typography>
+        <Typography sx={{ mt: 2, fontFamily: "Lato", fontSize: "18px" }}>
+          The HSPW committee members are recognized researchers in the oral
+          biology community. They are tasked to develop curation guidelines,
+          review annotations submitted by the community, and promote the wiki.
+          Click{" "}
+          <Link
+            to="/team"
+            style={{ textDecoration: "none" }}
+          >
+            here
+            <OpenInNewIcon
+              sx={{ fontSize: "medium", verticalAlign: "middle" }}
+            />
+          </Link>{" "}
+          to see who they are.
+        </Typography>
+        <Typography sx={{ mt: 2, fontFamily: "Lato", fontSize: "18px" }}>
+          To improve our efforts to a functional era of big data, our team has
+          joined the team from{" "}
           <a
             href="http://www.homd.org"
-            class="external text"
-            rel="nofollow"
             target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
           >
             HOMD
+            <OpenInNewIcon sx={{ fontSize: "medium" }} />
           </a>
           . We aim to integrate our datasets and bring our human derived data
           with microbial derived information. This will provide our teams with
           new knowledge of how and why complex host-microbial interactions
           govern health and disease.
+        </Typography>
+        <Typography sx={{ mt: 2, mb: 2, fontFamily: "Lato", fontSize: "18px" }}>
+          Thank you for your participation!
         </Typography>
       </Container>
     </>
