462b1a85a9049f4d3ce690b1779bd79cff896972
Mickey Zheng <mzheng@jcvi.org>
2023-12-28 18:59:03 -0500

    Added missing external link icons on salivary proteins page

--- src/pages/Browse/SalivaryProtein/SalivaryProtein.js
+++ src/pages/Browse/SalivaryProtein/SalivaryProtein.js
@@ -1,5 +1,7 @@
+import FontAwesome from "react-fontawesome";
+
 import Protein from "../../../components/SalivaryProtein/SalivaryProteinTable.js";
-import main_feature from "../../../assets/hero.jpeg";
+import MainFeature from "../../../assets/hero.jpeg";
 import "../../style.css";
 
 const SalivaryProtein = () => {
@@ -7,7 +9,7 @@ const SalivaryProtein = () => {
     <>
       <div
         style={{
-          backgroundImage: `url(${main_feature})`,
+          backgroundImage: `url(${MainFeature})`,
         }}
         className="head_background"
       >
@@ -20,28 +22,49 @@ const SalivaryProtein = () => {
         <p className="head_text">
           Proteins listed below have been manually reviewed and annotated by{" "}
           <a
+            target="_blank"
+            rel="noreferrer"
             href="https://www.uniprot.org/"
             className="linksa"
           >
-            UniProt
-          </a>
+            {"UniProt "}
+            <FontAwesome
+              className="super-crazy-colors"
+              name="external-link"
+              style={{ textShadow: "0 1px 0 rgba(0, 0, 0, 0.1)" }}
+            />
+          </a>{" "}
           , and have evidence of existence in human saliva through{" "}
           <a
+            target="_blank"
+            rel="noreferrer"
             href="https://en.wikipedia.org/wiki/Tandem_mass_spectrometry"
             className="linksa"
           >
-            tandem mass spectrometry (MS/MS)
+            {"tandem mass spectrometry (MS/MS) "}
+            <FontAwesome
+              className="super-crazy-colors"
+              name="external-link"
+              style={{ textShadow: "0 1px 0 rgba(0, 0, 0, 0.1)" }}
+            />
           </a>{" "}
           experiments using whole saliva or glandular secretion samples
           collected from healthy subjects. The list is updated automatically
           based on current supporting evidence derived from data uploaded to the
           wiki or retrieved from external sources, such as the{" "}
           <a
+            target="_blank"
+            rel="noreferrer"
             href="https://www.proteinatlas.org/"
             className="linksa"
           >
-            Human Protein Atlas
+            {"Human Protein Atlas "}
           </a>
+          <FontAwesome
+            className="super-crazy-colors"
+            name="external-link"
+            style={{ textShadow: "0 1px 0 rgba(0, 0, 0, 0.1)" }}
+          />
           .
         </p>
       </div>
