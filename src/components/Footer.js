4e5be7418449db722d4f5c7a1f57001048618326
Link Wu <iwu@jcvi.org>
2024-01-02 11:32:24 -0500

    Modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,148 +5,178 @@ import forsyth from "../assets/logo-forsyth.png";
 import uthsc from "../assets/logo-uthsc.svg";
 import ub from "../assets/logo-ub.png";
 import nih from "../assets/logo-nih-nidcr.png";
-import { Container } from "@mui/material";
+import { Container, Grid, Link, Typography } from "@mui/material";
 
 const Footer = () => {
+  const links = [
+    { href: "/about", text: "About HSPW" },
+    { href: "/accessibility", text: "Accessibility" },
+    { href: "/copyrights", text: "Copyrights" },
+    { href: "/disclaimers", text: "Disclaimers" },
+    { href: "/privacy-policy", text: "Privacy Notice" },
+  ];
+
   return (
-    <Container maxWidth="false">
-      <div id="footer">
-        <div className="footer-top">
-          <div className="container">
-            <div id="footer-logos">
-              <ul>
-                <li>
-                  <a
-                    href="http://www.jcvi.org"
-                    title="JCVI Homepage"
-                    alt="JCVI Homepage"
+    <Container maxWidth="xl">
+      <div id="footer-logos">
+        <ul>
+          <li>
+            <a
+              href="http://www.jcvi.org"
+              title="JCVI Homepage"
+              alt="JCVI Homepage"
+            >
+              <img
+                src={jcvi}
+                alt="JCVI Logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.forsyth.org"
+              title="The Forsyth Institute Homepage"
+              alt="The Forsyth Institute Homepage"
+            >
+              <img
+                src={forsyth}
+                alt="Forsyth Logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.uthsc.edu"
+              title="The University of Tennessee Health Science Center Homepage"
+              alt="The University of Tennessee Health Science Center Homepage"
+            >
+              <img
+                src={uthsc}
+                alt="University of Tennessee Health Center Logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.buffalo.edu"
+              title="University of Buffalo Homepage"
+              alt="University of Buffalo Homepage"
+            >
+              <img
+                src={ub}
+                alt="University of Buffalo Logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.nih.gov"
+              title="NIH Homepage"
+              alt="NIH Homepage"
+            >
+              <img
+                src={nih}
+                alt="NIH Logo"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="footer-text">
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            container
+            justifyContent="space-evenly"
+          >
+            {links.map((link, index) => (
+              <React.Fragment key={index}>
+                {index > 0 && (
+                  <Grid item>
+                    <Typography
+                      component="span"
+                      sx={{
+                        mx: 4,
+                        fontFamily: "Montserrat",
+                        color: "#6C6C6C",
+                      }}
+                    >
+                      |
+                    </Typography>
+                  </Grid>
+                )}
+                <Grid item>
+                  <Link
+                    href={link.href}
+                    underline="none"
                   >
-                    <img
-                      src={jcvi}
-                      alt="JCVI Logo"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://www.forsyth.org"
-                    title="The Forsyth Institute Homepage"
-                    alt="The Forsyth Institute Homepage"
-                  >
-                    <img
-                      src={forsyth}
-                      alt="Forsyth Logo"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://www.uthsc.edu"
-                    title="The University of Tennessee Health Science Center Homepage"
-                    alt="The University of Tennessee Health Science Center Homepage"
-                  >
-                    <img
-                      src={uthsc}
-                      alt="University of Tennessee Health Center Logo"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://www.buffalo.edu"
-                    title="University of Buffalo Homepage"
-                    alt="University of Buffalo Homepage"
-                  >
-                    <img
-                      src={ub}
-                      alt="University of Buffalo Logo"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="http://www.nih.gov"
-                    title="NIH Homepage"
-                    alt="NIH Homepage"
-                  >
-                    <img
-                      src={nih}
-                      alt="NIH Logo"
-                    />
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div id="footer-text">
-              <ul
-                id="last_sentence"
-                style={{ marginBottom: "45px" }}
+                    {link.text}
+                  </Link>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+        <div
+          style={{
+            height: "3px",
+            background: "#C9C9C9",
+            margin: "30px 0",
+          }}
+        ></div>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            container
+            justifyContent="center"
+            sx={{ ml: 4, mr: 4 }}
+          >
+            <Grid item>
+              <p className="address last">
+                Project is funded by NIDCR under grant R01 DE016937-16
+              </p>
+            </Grid>
+          </Grid>
+        </Grid>
+        <div
+          style={{
+            height: "3px",
+            background: "#C9C9C9",
+            margin: "30px 0",
+          }}
+        ></div>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            container
+            justifyContent="space-between"
+            sx={{ ml: 2, mr: 2, mb: 4 }}
+          >
+            <Grid item>
+              <p className="address last">
+                By J. Craig Venter Institute, 4120 Capricorn Lane, La Jolla, CA
+                92037
+              </p>
+            </Grid>
+            <Grid item>
+              <Link
+                href="/contact"
+                underline="none"
               >
-                <li className="last">
-                  Project is funded by NIDCR under grant R01 DE016937-16
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  <a
-                    href="about"
-                    className="first_li"
-                  >
-                    About HSPW
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/accessibility"
-                    className="middle"
-                  >
-                    Accessibility
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/copyrights"
-                    className="middle"
-                  >
-                    Copyrights
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/disclaimers"
-                    className="middle"
-                  >
-                    Disclaimers
-                  </a>
-                </li>
-                <li className="last">
-                  <a
-                    href="/privacy-policy"
-                    className="last_li"
-                  >
-                    Privacy Notice
-                  </a>
-                </li>
-              </ul>
-              <hr
-                className="solid"
-                style={{ marginTop: "35px", marginBottom: "35px" }}
-              ></hr>
-              <ul>
-                <li className="address last">
-                  By J. Craig Venter Institute, 4120 Capricorn Lane, La Jolla,
-                  CA 92037
-                </li>
-                <li
-                  className="last"
-                  style={{ marginLeft: "37%" }}
-                >
-                  <a href="/contact">Contact Us</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+                <p className="address last">Contact Us</p>
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     </Container>
   );
