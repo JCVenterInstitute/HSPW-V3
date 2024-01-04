856a408dd80a1980f5a5685c18b8dceba08860e2
Link Wu <iwu@jcvi.org>
2024-01-04 16:09:13 -0500

    Modified analysis home page and removed background of the images

--- src/pages/AnalysisHome.js
+++ src/pages/AnalysisHome.js
@@ -1,11 +1,6 @@
 import React from "react";
 import main_feature from "../assets/hero.jpeg";
-import filtering from "../assets/icon-filtering.png";
-import normalization from "../assets/icon-normalization.png";
-import heatmap from "../assets/icon-heatmap.png";
-import ranking from "../assets/icon-ranking.png";
-import volcano_plot from "../assets/icon-volcano-plot.png";
-import network_analysis from "../assets/icon-network-analysis.png";
+import differential from "../assets/icon-heatmap.png";
 import BasicCard from "../components/BasicCard";
 import { StyledEngineProvider } from "@mui/material/styles";
 
@@ -25,47 +20,73 @@ class analysisHome extends React.Component {
           },
           basicCards: [
             {
-              size: "1",
-              imageSrc: filtering,
-              title: "Filtering",
-              blurb:
-                "Improve quality of raw data collected from various sources.",
-            },
-            {
-              size: "1",
-              imageSrc: normalization,
-              title: "Normalization",
-              blurb:
-                "Adjust values measured on different scales to a notionally common scale.",
-            },
-            {
-              size: "1",
-              imageSrc: heatmap,
-              title: "Heatmap",
-              blurb:
-                "Shows the correlation of protein abundance between two sets.",
-            },
-            {
-              size: "1",
-              imageSrc: ranking,
-              title: "Ranking",
-              blurb:
-                "Use ranking scores to find the correlation of protein abundance between two sets.",
-            },
-            {
-              size: "1",
-              imageSrc: volcano_plot,
-              title: "Volcano Plot",
+              imageSrc: differential,
+              title: "Multiple Sequence Alignment",
               blurb:
                 "Shows the difference in protein abundance between samples in Groups A and B.",
             },
             {
-              size: "1",
-              imageSrc: network_analysis,
-              title: "Network Analysis",
-              blurb: "Find correlated protein in our dataset.",
+              imageSrc: differential,
+              title: "Differential Expression",
+              blurb:
+                "Shows the difference in protein abundance between samples in Groups A and B.",
+            },
+            {
+              imageSrc: differential,
+              title: "Protein Signature Search",
+              blurb:
+                "Shows the difference in protein abundance between samples in Groups A and B.",
+            },
+            {
+              imageSrc: differential,
+              title: "Protein Similarity Search",
+              blurb:
+                "Shows the difference in protein abundance between samples in Groups A and B.",
             },
           ],
+          // basicCards: [
+          //   {
+          //     // size: "1",
+          //     imageSrc: filtering,
+          //     title: "Filtering",
+          //     blurb:
+          //       "Improve quality of raw data collected from various sources.",
+          //   },
+          //   {
+          //     // size: "1",
+          //     imageSrc: normalization,
+          //     title: "Normalization",
+          //     blurb:
+          //       "Adjust values measured on different scales to a notionally common scale.",
+          //   },
+          //   {
+          //     // size: "1",
+          //     imageSrc: heatmap,
+          //     title: "Heatmap",
+          //     blurb:
+          //       "Shows the correlation of protein abundance between two sets.",
+          //   },
+          //   {
+          //     // size: "1",
+          //     imageSrc: ranking,
+          //     title: "Ranking",
+          //     blurb:
+          //       "Use ranking scores to find the correlation of protein abundance between two sets.",
+          //   },
+          //   {
+          //     // size: "1",
+          //     imageSrc: volcano_plot,
+          //     title: "Volcano Plot",
+          //     blurb:
+          //       "Shows the difference in protein abundance between samples in Groups A and B.",
+          //   },
+          //   {
+          //     // size: "1",
+          //     imageSrc: network_analysis,
+          //     title: "Network Analysis",
+          //     blurb: "Find correlated protein in our dataset.",
+          //   },
+          // ],
         },
       },
     };
