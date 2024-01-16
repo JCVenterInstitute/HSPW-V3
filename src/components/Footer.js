7c84656f04c238dce1404dd7a748f444de82be81
Mickey Zheng <mzheng@jcvi.org>
2024-01-16 11:39:52 -0500

    Updated footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import forsyth from "../assets/logo-forsyth.png";
 import uthsc from "../assets/logo-uthsc.svg";
 import ub from "../assets/logo-ub.png";
 import nih from "../assets/logo-nih-nidcr.png";
-import { Container, Grid, Link, Typography } from "@mui/material";
+import { Container, Grid, Link } from "@mui/material";
 
 const Footer = () => {
   const links = [
@@ -17,110 +17,101 @@ const Footer = () => {
     { href: "/contact", text: "Contact Us" },
   ];
 
+  const logos = [
+    {
+      href: "http://www.jcvi.org",
+      title: "JCVI Homepage",
+      altText: "JCVI Homepage",
+      image: jcvi,
+    },
+    {
+      href: "http://www.forsyth.org",
+      title: "The Forsyth Institute Homepage",
+      altText: "The Forsyth Institute Homepage",
+      image: forsyth,
+    },
+    {
+      href: "http://www.uthsc.edu",
+      title: "The University of Tennessee Health Science Center Homepage",
+      altText: "The University of Tennessee Health Science Center Homepage",
+      image: uthsc,
+    },
+    {
+      href: "http://www.buffalo.edu",
+      title: "University of Buffalo Homepage",
+      altText: "University of Buffalo Homepage",
+      image: ub,
+    },
+    {
+      href: "http://www.nih.gov",
+      title: "NIH Homepage",
+      altText: "NIH Homepage",
+      image: nih,
+    },
+  ];
+
   return (
     <div className="footer-container">
       <Container maxWidth="xl">
-        <div id="footer-logos">
-          <ul>
-            <li>
-              <a
-                href="http://www.jcvi.org"
-                title="JCVI Homepage"
-                alt="JCVI Homepage"
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            justifyContent: "space-evenly",
+          }}
+        >
+          {logos.map((logo) => {
+            return (
+              <Grid
+                item
+                xs={6}
+                sm={4}
+                md={4}
+                lg={2}
+                className="logo-grid"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
               >
-                <img
-                  src={jcvi}
-                  alt="JCVI Logo"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://www.forsyth.org"
-                title="The Forsyth Institute Homepage"
-                alt="The Forsyth Institute Homepage"
-              >
-                <img
-                  src={forsyth}
-                  alt="Forsyth Logo"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://www.uthsc.edu"
-                title="The University of Tennessee Health Science Center Homepage"
-                alt="The University of Tennessee Health Science Center Homepage"
-              >
-                <img
-                  src={uthsc}
-                  alt="University of Tennessee Health Center Logo"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://www.buffalo.edu"
-                title="University of Buffalo Homepage"
-                alt="University of Buffalo Homepage"
-              >
-                <img
-                  src={ub}
-                  alt="University of Buffalo Logo"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://www.nih.gov"
-                title="NIH Homepage"
-                alt="NIH Homepage"
-              >
-                <img
-                  src={nih}
-                  alt="NIH Logo"
-                />
-              </a>
-            </li>
-          </ul>
-        </div>
+                <Link
+                  href={logo.href}
+                  target="_blank"
+                >
+                  <img
+                    src={logo.image}
+                    alt={logo.altText}
+                  />
+                </Link>
+              </Grid>
+            );
+          })}
+        </Grid>
         <div id="footer-text">
           <Grid
             container
             spacing={3}
           >
-            <Grid
-              item
-              container
-              justifyContent="space-evenly"
-            >
-              {links.map((link, index) => (
-                <React.Fragment key={index}>
-                  {index > 0 && (
-                    <Grid item>
-                      <Typography
-                        component="span"
-                        sx={{
-                          mx: 4,
-                          fontFamily: "Montserrat",
-                          color: "#6C6C6C",
-                        }}
-                      >
-                        |
-                      </Typography>
-                    </Grid>
-                  )}
-                  <Grid item>
-                    <Link
-                      href={link.href}
-                      underline="none"
-                    >
-                      {link.text}
-                    </Link>
-                  </Grid>
-                </React.Fragment>
-              ))}
-            </Grid>
+            {links.map((link, index) => (
+              <Grid
+                item
+                container
+                justifyContent="space-evenly"
+                xs={6}
+                sm={4}
+                md={4}
+                lg={2}
+              >
+                <Link
+                  href={link.href}
+                  underline="none"
+                  style={{ fontSize: "16px" }}
+                >
+                  {link.text}
+                </Link>
+              </Grid>
+            ))}
           </Grid>
           <div
             style={{
