0099427590e3d4bed5137719639ebd336159a45a
Mickey Zheng <mzheng@jcvi.org>
2024-01-16 11:58:29 -0500

    Updated analysis home card layout

--- src/pages/AnalysisHome.js
+++ src/pages/AnalysisHome.js
@@ -4,9 +4,15 @@ import differential from "../assets/icon-heatmap.png";
 import msa from "../assets/icon-msa-alignment.png";
 import blast from "../assets/icon-protein-blast.png";
 import signature from "../assets/icon-protein-signature.png";
-import BasicCard from "../components/BasicCard";
 import { StyledEngineProvider } from "@mui/material/styles";
-import { Container } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 class analysisHome extends React.Component {
   constructor(props) {
@@ -52,49 +58,6 @@ class analysisHome extends React.Component {
               location: "/psiblast",
             },
           ],
-          // basicCards: [
-          //   {
-          //     // size: "1",
-          //     imageSrc: filtering,
-          //     title: "Filtering",
-          //     blurb:
-          //       "Improve quality of raw data collected from various sources.",
-          //   },
-          //   {
-          //     // size: "1",
-          //     imageSrc: normalization,
-          //     title: "Normalization",
-          //     blurb:
-          //       "Adjust values measured on different scales to a notionally common scale.",
-          //   },
-          //   {
-          //     // size: "1",
-          //     imageSrc: heatmap,
-          //     title: "Heatmap",
-          //     blurb:
-          //       "Shows the correlation of protein abundance between two sets.",
-          //   },
-          //   {
-          //     // size: "1",
-          //     imageSrc: ranking,
-          //     title: "Ranking",
-          //     blurb:
-          //       "Use ranking scores to find the correlation of protein abundance between two sets.",
-          //   },
-          //   {
-          //     // size: "1",
-          //     imageSrc: volcano_plot,
-          //     title: "Volcano Plot",
-          //     blurb:
-          //       "Shows the difference in protein abundance between samples in Groups A and B.",
-          //   },
-          //   {
-          //     // size: "1",
-          //     imageSrc: network_analysis,
-          //     title: "Network Analysis",
-          //     blurb: "Find correlated protein in our dataset.",
-          //   },
-          // ],
         },
       },
     };
@@ -124,25 +87,55 @@ class analysisHome extends React.Component {
             id="application"
             data-layout={page.layout}
           >
-            <Container maxWidth="xl">
-              <div id="page-content">
-                <div className="basic-card-container">
-                  {page.basicCards.map((props, i) => {
-                    return (
-                      <BasicCard
-                        key={i}
-                        rawContent={props.rawContent}
-                        size={props.size}
-                        destination={props.destination}
-                        imageSrc={props.imageSrc}
-                        title={props.title}
-                        blurb={props.blurb}
-                        location={props.location}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
+            <Container
+              maxWidth="xl"
+              sx={{ marginY: "30px" }}
+            >
+              <Grid
+                container
+                spacing={3}
+              >
+                {page.basicCards.map((props, i) => {
+                  return (
+                    <Grid
+                      item
+                      xs={12}
+                      sm={6}
+                      md={6}
+                      lg={3}
+                    >
+                      <Card
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          height: "100%",
+                          minHeight: "400px",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <CardContent>
+                          <Typography
+                            variant="h6"
+                            component="div"
+                          >
+                            {props.title}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="text.secondary"
+                          >
+                            {props.blurb}
+                          </Typography>
+                        </CardContent>
+                        <CardMedia
+                          component={"img"}
+                          image={props.imageSrc}
+                        />
+                      </Card>
+                    </Grid>
+                  );
+                })}
+              </Grid>
             </Container>
           </div>
         </StyledEngineProvider>
