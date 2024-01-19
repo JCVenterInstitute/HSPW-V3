627b1ed21b77335fd49b4cf1dae2c36188762b00
Link Wu <iwu@jcvi.org>
2024-01-19 08:44:36 -0500

    Merged carousel

--- src/components/CarouselData.js
+++ src/components/CarouselData.js
@@ -7,6 +7,8 @@ export const contents = [
     index: 0,
     subtitle:
       "The Human Salivary Proteome (HSP) is a collaborative, community-based Web portal to more than 1,000 unique human saliva proteins identified by high-throughput proteomic technologies. The HSP is developed for the research community and the public to harness the knowledge in the data and to further enhance the value of the proteome. You are very welcome to share your thoughts in the forums, add your own data to the growing database, annotate the proteins, or just explore the site.",
+    subtitle1:
+      "The chord diagram on left provides salivary protein catalog with each arc in the diagram represents the set of proteins found in the connected tissue or sample types. Click on Chord image to go to salivary protein catalog page that provides the interactive chord diagram and salivary protein search capability.",
     img: first_bg,
   },
   {
@@ -15,12 +17,5 @@ export const contents = [
     subtitle:
       "Via shotgun proteomics, we have mapped salivary proteomes of COVID-19 and healthy subjects. We aim to understand further the molecular cues of the virus on the context of health and disease.",
     img: second_bg,
-  },
-  {
-    index: 2,
-    title: "Salivary Protein Map",
-    subtitle:
-      "The chord diagram on left provides salivary protein catalog with each arc in the diagram represents the set of proteins found in the connected tissue or sample types. Click on Chord image to go to salivary protein catalog page that provides the interactive chord diagram and salivary protein search capability.",
-    img: third_bg,
   },
 ];
