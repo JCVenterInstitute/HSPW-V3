aada2d6db6b61fb12a03de570141e6bb9f559ce3
Link Wu <iwu@jcvi.org>
2024-02-14 12:14:15 -0500

    Removed .env in server and added env variable in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,24 @@ module.exports = {
     {
       name: "hspw",
       script: "./src/server/server.js",
-      instances: "max",
+      instances: "2",
       exec_mode: "cluster",
+      env_dev: {
+        SMTP_USER: "/CONTACT/USER",
+        SMTP_PASSWORD: "/CONTACT/PASSWORD",
+        RECAPTCHA_SECRET_KEY: "/RECAPTCHA/SECRET_KEY",
+        DIFFERENTIAL_S3_BUCKET: "hspw-data-dev-v2",
+        R_SCRIPT_PATH: "/home/ec2-user/r4test1/test",
+        CONTACT_TABLE: "hspw-dev-contact-detail",
+      },
+      env_prod: {
+        SMTP_USER: "/CONTACT/USER",
+        SMTP_PASSWORD: "/CONTACT/PASSWORD",
+        RECAPTCHA_SECRET_KEY: "/RECAPTCHA/SECRET_KEY",
+        DIFFERENTIAL_S3_BUCKET: "hspw-data-prod-v2",
+        R_SCRIPT_PATH: "/home/ec2-user/r4test1/test",
+        CONTACT_TABLE: "hspw-prod-contact-detail",
+      },
     },
   ],
 };
