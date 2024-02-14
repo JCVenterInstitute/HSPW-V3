715d14de0b5d4687fd10194a94e673fe0e7e44b9
Link Wu <iwu@jcvi.org>
2024-02-14 09:29:19 -0500

    Updated config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,16 @@ module.exports = {
       script: "./src/server/server.js",
       instances: "max",
       exec_mode: "cluster",
-      env_production: {
-        NODE_ENV: "prod",
-      },
-      env_development: {
+      env_dev: {
         NODE_ENV: "dev",
+        SMTP_USER: "/CONTACT/USER",
+        SMTP_PASSWORD: "/CONTACT/PASSWORD",
+        RECAPTCHA_SECRET_KEY: "/RECAPTCHA/SECRET_KEY",
+        DIFFERENTIAL_S3_BUCKET: "hspw-data-dev-v2",
+        R_SCRIPT_PATH: "/home/ec2-user/r4test1/test",
+      },
+      env_prod: {
+        NODE_ENV: "prod",
       },
     },
   ],
